01279f1343e8483308de9a166702768ad9810e08
vitalinvent <vitalinvent@hotmail.com>
2016-09-16 10:03:48 +0300

    Добавьте файлы проекта.

---
+++ extractor7z/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace extractor7z
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
