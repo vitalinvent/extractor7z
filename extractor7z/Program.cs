2c80a07f556c032bd05d21ffa05e1923f254e934
vitalinvent <vitalinvent@gmail.com>
2016-09-16 21:02:46 +0300

    make try for errors

--- extractor7z/Program.cs
+++ extractor7z/Program.cs
@@ -21,52 +21,82 @@ namespace extractor7z
             if (!Directory.Exists("extracted")) Directory.CreateDirectory("extracted");
             if (args.Length > 0)
             {
-                string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), args[0]);
-                //string[] files = Directory.GetFiles("X:\\temp ", "1.7z ");
-                Console.WriteLine("Found " + files.Length + " files");
-                int countArchivesExtracted = 0;
-                if (args.Length > 1)
+                try
                 {
-                    if (args[1].Equals("exclude"))
+                    string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), args[0]);
+                    //string[] files = Directory.GetFiles("X:\\temp ", "1.7z ");
+                    Console.WriteLine("Found " + files.Length + " files");
+                    int countArchivesExtracted = 0;
+                    if (args.Length > 1)
                     {
-                        arrExclude = args[2].Split(',');
-                    }
-                }
-                foreach (string file in files)
-                {
-                    zip = new SevenZip.SevenZipExtractor(file);
-                    foreach (string fileArc in zip.ArchiveFileNames)
-                    {
-                        size = 0;
-                        idx = 0;
-                        name = "";
-                        //ReadOnlyCollection<SevenZip.ArchiveFileInfo> infos = ;
-                        foreach (SevenZip.ArchiveFileInfo info in zip.ArchiveFileData)
+                        if (args[1].Equals("exclude"))
                         {
-                            string fileName =info.FileName;
-                            bool exist = false;
-                            foreach (string exclude in arrExclude)
-                            {
-                                if (fileName.ToLower().IndexOf(exclude.ToLower()) > 0) exist = true;
-                            }
-                            if ((size < info.Size) & (exist == false))
-                            {
-                                idx = info.Index;
-                                name = fileName;
-                            }
-
+                            arrExclude = args[2].Split(',');
                         }
-                        //using (FileStream fs = File.OpenWrite(Directory.))
                     }
-                    using (FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\extracted\\" + name, FileMode.Create))
+                    foreach (string file in files)
                     {
-                        Console.Write("\r {0} of {1} Archive: {2} File: {3}", (countArchivesExtracted + 1).ToString(), files.Length.ToString(), file, name);
-                        zip.ExtractFile(idx, fs);
-                        countArchivesExtracted++;
+                        zip = new SevenZip.SevenZipExtractor(file);
+                        try
+                        {
+                            foreach (string fileArc in zip.ArchiveFileNames)
+                            {
+                                size = 0;
+                                try
+                                {
+                                    idx = 0;
+                                    name = "";
+                                    //ReadOnlyCollection<SevenZip.ArchiveFileInfo> infos = ;
+                                    foreach (SevenZip.ArchiveFileInfo info in zip.ArchiveFileData)
+                                    {
+                                        try
+                                        {
+                                            string fileName = info.FileName;
+                                            bool exist = false;
+                                            foreach (string exclude in arrExclude)
+                                            {
+                                                if (fileName.ToLower().IndexOf(exclude.ToLower()) > 0) exist = true;
+                                            }
+                                            if ((size < info.Size) & (exist == false))
+                                            {
+                                                idx = info.Index;
+                                                name = fileName;
+                                            }
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            Console.WriteLine(ex.Message);
+                                        }
+
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+
+                                //using (FileStream fs = File.OpenWrite(Directory.))
+                            }
+                            using (FileStream fs = new FileStream(Directory.GetCurrentDirectory() + "\\extracted\\" + name, FileMode.Create))
+                            {
+                                Console.Write("\r {0} of {1} Archive: {2} File: {3}", (countArchivesExtracted + 1).ToString(), files.Length.ToString(), file, name);
+                                zip.ExtractFile(idx, fs);
+                                countArchivesExtracted++;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
                     }
+                    Console.WriteLine("");
+                    Console.WriteLine("Extracted: " + countArchivesExtracted.ToString());
                 }
-                Console.WriteLine("");
-                Console.WriteLine("Extracted: " + countArchivesExtracted.ToString());
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
             else
             {
